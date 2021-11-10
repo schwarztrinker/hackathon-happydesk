92d90022902159ecef5a3fb4ff32604eecd7ab55
Martin Stöcker <martin.stoecker99@gmail.com>
2021-11-10 12:53:54 +0100

    int commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LayoutComponent,
+    children: [
+      {path: '',component: WelcomeMessageComponent},
+      {path: 'preferences', component: PreferencesComponent}
+  ]},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    WelcomeMessageComponent,
+    FooterComponent,
+    NavbarComponent,
+    PreferencesComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
