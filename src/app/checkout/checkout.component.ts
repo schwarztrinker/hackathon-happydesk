9e067c6551a5cbfe1fba2b650a7f554176cb5f47
Martin Stöcker <martin.stoecker99@gmail.com>
2021-11-16 11:12:26 +0100

    fixed checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dialogue',
-  templateUrl: './dialogue.component.html',
-  styleUrls: ['./dialogue.component.css']
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
 })
-export class DialogueComponent implements OnInit {
+export class CheckoutComponent implements OnInit {
 
   constructor() { }
 
