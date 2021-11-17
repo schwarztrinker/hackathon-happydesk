5332eba7d3668fdec12db4aa7c5bdae48549d2f3
Martin Stöcker <martin.stoecker99@gmail.com>
2021-11-17 11:34:05 +0100

    refactoring and dialogue

--- src/app/dialogue/dialogue.component.ts
+++ src/app/dialogue/dialogue.component.ts
@@ -11,12 +11,12 @@ import { Desk } from '../classes/desk';
 export class DialogueComponent {
   deskService: DeskService;
 
-  public filter = { moreThanTwoMonitors: false, ergonomicMouse: false,  dockingstation: false, window: false, singleOffice: false, available: false };
+  public filter = { moreThanTwoMonitors: false, ergonomicMouse: false, dockingstation: false, window: false, singleOffice: false, available: false };
 
 
   filteredDesks: Desk[] = [];
 
-  constructor(loginService: LoginService) { 
+  constructor(loginService: LoginService) {
     this.deskService = new DeskService
     this.filteredDesks = this.deskService.desks
     loginService.login()
@@ -26,23 +26,23 @@ export class DialogueComponent {
   filterChange() {
     this.filteredDesks = this.deskService.desks
 
-    if(this.filter.moreThanTwoMonitors){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.moreThanTwoMonitors})
+    if (this.filter.moreThanTwoMonitors) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.moreThanTwoMonitors })
     }
-    if(this.filter.ergonomicMouse){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.ergonomicMouse})
+    if (this.filter.ergonomicMouse) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.ergonomicMouse })
     }
-    if(this.filter.dockingstation){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.dockingstation})
+    if (this.filter.dockingstation) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.dockingstation })
     }
-    if(this.filter.window){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.window})
+    if (this.filter.window) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.window })
     }
-    if(this.filter.singleOffice){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.singleOffice})
+    if (this.filter.singleOffice) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.singleOffice })
     }
-    if(this.filter.available){
-      this.filteredDesks = this.filteredDesks.filter(desk => {return desk.available})
+    if (this.filter.available) {
+      this.filteredDesks = this.filteredDesks.filter(desk => { return desk.available })
     }
   }
 
@@ -51,29 +51,29 @@ export class DialogueComponent {
 
   // Predictions for Filtering 
 
-  predictMoreThanTwoMonitors(){
-    return this.filteredDesks.filter(desk => {return desk.moreThanTwoMonitors}).length
+  predictMoreThanTwoMonitors() {
+    return this.filteredDesks.filter(desk => { return desk.moreThanTwoMonitors }).length
   }
 
-  predictErgonomicMouse(){
-    return this.filteredDesks.filter(desk => {return desk.ergonomicMouse}).length
+  predictErgonomicMouse() {
+    return this.filteredDesks.filter(desk => { return desk.ergonomicMouse }).length
   }
 
-  predictDockingStation(){
-    return this.filteredDesks.filter(desk => {return desk.dockingstation}).length
+  predictDockingStation() {
+    return this.filteredDesks.filter(desk => { return desk.dockingstation }).length
   }
 
-  predictWindow(){
-    return this.filteredDesks.filter(desk => {return desk.window}).length
+  predictWindow() {
+    return this.filteredDesks.filter(desk => { return desk.window }).length
   }
 
-  predictSingleOffice(){
-    return this.filteredDesks.filter(desk => {return desk.singleOffice}).length
+  predictSingleOffice() {
+    return this.filteredDesks.filter(desk => { return desk.singleOffice }).length
   }
 
 
 
 
-  
+
 
 }
